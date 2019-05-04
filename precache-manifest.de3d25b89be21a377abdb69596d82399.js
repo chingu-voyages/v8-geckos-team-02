388884c26ed27b8babaf541682e2b9efafd46b00
Shahzad <mshahzadreza@gmail.com>
2019-05-04 19:58:45 +0600

    Updates

---
+++ precache-manifest.de3d25b89be21a377abdb69596d82399.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1174afeedce2a6d2faed",
+    "url": "/v8-geckos-team-02/static/js/runtime~main.1174afee.js"
+  },
+  {
+    "revision": "fffa5b1aa84ecc7d54a5",
+    "url": "/v8-geckos-team-02/static/js/main.fffa5b1a.chunk.js"
+  },
+  {
+    "revision": "717a9a49881b9fadaf26",
+    "url": "/v8-geckos-team-02/static/js/1.717a9a49.chunk.js"
+  },
+  {
+    "revision": "717a9a49881b9fadaf26",
+    "url": "/v8-geckos-team-02/static/css/1.9f1e97e9.chunk.css"
+  },
+  {
+    "revision": "055cc8595bb67291b95a3be39bfd1868",
+    "url": "/v8-geckos-team-02/index.html"
+  }
+];
